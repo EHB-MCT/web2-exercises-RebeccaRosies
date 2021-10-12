fd036985d2283482a2797f6f421788db803cf2c5
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-10-12 17:43:54 +0200

    show movie info based on form input

--- Class3/ex2_timeWasted/index.js
+++ Class3/ex2_timeWasted/index.js
@@ -1,14 +1,44 @@
 const apiKey  = "4a76b54";
+const form = document.getElementById("searchform");
+function initfields(){   
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let inputTitle = document.getElementById("inputTitle").value
+        console.log(inputTitle);
+        fetchMovies(inputTitle);
+    })
+}
 
-function fetchMovies(){
-    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=parasite`)
+function fetchMovies(Title){
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${Title}`)
     .then(response => response.json())
-    .then(data => 
-        console.log(data))
+    .then(data => {
+        console.log(data);
+        /* showMovies(data) */
+        form.insertAdjacentHTML("afterend", `
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="${data.Poster}" class="card-img" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${data.Title}</h5>
+                            <p class="card-text">${data.Plot}</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <p class="card-text"> RunTime = ${data.Runtime} </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `)
+        
+    
+    })
 };
 
 window.onload = function(){
-    fetchMovies();
+    initfields();
 };
 
 
